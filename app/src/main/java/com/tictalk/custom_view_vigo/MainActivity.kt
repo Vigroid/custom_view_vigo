e2d1232a9ca0db6a744cb3378dbc18508501f7c9
weikang yang <yangvigo@tictalk.com>
2019-03-01 19:20:03 +0800

    波浪view 输入框view黑点开发完成，对字数有限制

--- app/src/main/java/com/tictalk/custom_view_vigo/MainActivity.kt
+++ app/src/main/java/com/tictalk/custom_view_vigo/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.tictalk.custom_view_vigo
 
-import android.graphics.PointF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.tictalk.widget.BubbleBean
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        confirm.setOnClickListener {
-            confirm.startAnim()
-        }
     }
 }
--- widget/src/main/java/com/tictalk/widget/InputRectView.kt
+++ widget/src/main/java/com/tictalk/widget/InputRectView.kt
@@ -1,4 +1,42 @@
 package com.tictalk.widget
 
-class InputRectView {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.text.InputFilter
+import android.util.AttributeSet
+import android.widget.EditText
+
+class InputRectView(context: Context, attributeSet: AttributeSet) : EditText(context, attributeSet) {
+
+    private var textLength = 0
+    private val maxTextLength = 6
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    init {
+        paint.style = Paint.Style.FILL
+        paint.color = Color.BLACK
+        filters = arrayOf(InputFilter.LengthFilter(maxTextLength))
+        setBackgroundColor(Color.TRANSPARENT)
+        isCursorVisible = false
+    }
+
+    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
+        textLength = Math.min(text?.length ?: 0, maxTextLength)
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.let { drawPswCircles(it) }
+    }
+
+    private fun drawPswCircles(canvas: Canvas) {
+        val drawingHeight = height / 2f
+        val drawingWidth = width / (maxTextLength+1).toFloat()
+
+        for (i in 1..textLength) {
+            canvas.drawCircle(drawingWidth*i, drawingHeight, 20f, paint)
+        }
+    }
 }
